9a1717586d8b7df4d214c8772073dc41da593997
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2021-12-14 19:10:21 +0900

    ログインユーザー情報をContextに保持してみる

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -4,6 +4,7 @@ import { Center, Spinner, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers"
 import { useSelectUser } from "../../hooks/useSelectUser"
+import { useLoginUser } from "../../hooks/useLoginUser"
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
 
@@ -11,6 +12,9 @@ export const UserManagement: VFC = memo(() => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const { getUsers, users, loading } = useAllUsers()
     const { onSelectUser, selectedUser } = useSelectUser()
+    const { loginUser } = useLoginUser()
+
+    console.log(loginUser)
 
     useEffect(() => getUsers(), [])
 
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -4,22 +4,30 @@ import { useCallback, useState } from "react"
 import { useHistory } from "react-router-dom"
 
 import { User } from "../types/api/user"
+import { useLoginUser } from "./useLoginUser"
 import { useMasage } from "./useMasage"
 
 export const useAuth = () => {
     const histry = useHistory()
     const { showMasage } = useMasage()
+    const { setLoginUser } = useLoginUser()
+
     const [loading, setLoading] = useState(false)
     const login = useCallback((id: string) => {
         setLoading(true)
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
             if(res.data){
+                setLoginUser(res.data)
                 showMasage({title: "ログインしました", status: "success"})
                 histry.push("/home")
             }else{
                 showMasage({title: "ユーザーが見つかりません", status: "error"})
+                setLoading(false)
             }
-        }).catch(() => showMasage({title: "ログインできません", status: "error"})).finally(() => setLoading(false))
-    }, [histry, showMasage])
+        }).catch(() => {
+            showMasage({title: "ログインできません", status: "error"})
+            setLoading(false)
+        })
+    }, [histry, showMasage, setLoginUser])
     return { login, loading }
 }
