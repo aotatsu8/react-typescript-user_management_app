922956950b3bd307c9131d9555bd2c8509de9652
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2021-12-15 15:55:44 +0900

    管理者ユーザーのみユーザー情報を編集できる導線の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -14,8 +14,6 @@ export const UserManagement: VFC = memo(() => {
     const { onSelectUser, selectedUser } = useSelectUser()
     const { loginUser } = useLoginUser()
 
-    console.log(loginUser)
-
     useEffect(() => getUsers(), [])
 
     const onClickUser = useCallback((id: number) => {
@@ -43,7 +41,7 @@ export const UserManagement: VFC = memo(() => {
                     ))}
                 </Wrap>
             )}
-            <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+            <UserDetailModal user={selectedUser} isOpen={isOpen} isAdmin={loginUser?.isAdmin} onClose={onClose} />
         </>
     )
 })
